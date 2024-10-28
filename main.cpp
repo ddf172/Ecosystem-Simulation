9934fda2790e6a26ecb9fa6bb08055cf2c8b53c1
ddf172 <jakubddfadamski@gmail.com>
2024-10-28 21:11:10 +0100

    Starting commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ddf on 28.10.2024.
+//
+
+#include "main.h"
+#include <iostream>
+
+int main() {
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by ddf on 28.10.2024.
+//
+
+#ifndef ECOSYSTEM_SIMULATION_MAIN_H
+#define ECOSYSTEM_SIMULATION_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //ECOSYSTEM_SIMULATION_MAIN_H
