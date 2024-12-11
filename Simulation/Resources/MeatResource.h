4e32d4a13d263513a4f9fb4d21c6df8490330d19
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2024-12-11 23:10:49 +0100

    Added MeatResource

---
+++ Simulation/Resources/MeatResource.h
@@ -0,0 +1,18 @@
+//
+// Created by Piotrek on 11.12.2024.
+//
+
+#ifndef MEATRESOURCE_H
+#define MEATRESOURCE_H
+#include "Resource.h"
+
+
+class MeatResource: public Resource  {
+
+public:
+    MeatResource(int amount, int energyValue);
+};
+
+
+
+#endif //MEATRESOURCE_H
