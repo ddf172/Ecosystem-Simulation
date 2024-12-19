c80c7f62b2ef350785eade612fdae7b6afb1f28e
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2024-12-19 15:36:34 +0100

    Added TileTests.cpp

--- GTests/TileTests.cpp
+++ GTests/TileTests.cpp
@@ -1,3 +1,29 @@
 //
 // Created by Piotr on 19.12.2024.
 //
+#include <gtest/gtest.h>
+#include "General/Tile.h"
+#include "Simulation/Animals/HerbivoreAnimal.h"
+
+TEST(TileTest, TileCreationTest) {
+    Tile* tile = new Tile(1, 1);
+    EXPECT_EQ(tile->getX(), 1);
+    EXPECT_EQ(tile->getX(), 1);
+    EXPECT_NE(tile, nullptr);
+    delete tile;
+}
+
+TEST(TileTest, TileAnimals) {
+    Tile tile = Tile(1, 1);
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 0);
+    std::shared_ptr<Animal> firstAnimal = std::make_shared<HerbivoreAnimal>(1, 1, 1);
+    tile.addAnimalOnTile(firstAnimal);
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 1);
+    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(1, 1, 1));
+    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(1, 1, 1));
+    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(1, 1, 1));
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 4);
+    tile.removeAnimalOnTile(firstAnimal);
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 3);
+}
+
