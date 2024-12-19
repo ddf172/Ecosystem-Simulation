75a65ad1bf67724e8710ca4a3f0dafce1d0f8307
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2024-12-19 19:06:05 +0100

    TileTests finished

--- GTests/TileTests.cpp
+++ GTests/TileTests.cpp
@@ -13,17 +13,48 @@ TEST(TileTest, TileCreationTest) {
     delete tile;
 }
 
-TEST(TileTest, TileAnimals) {
+TEST(TileTest, TileAddingAnimals) {
+    Tile tile = Tile(1, 1);
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 0);
+    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(1, 1, 1));
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 1);
+    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(2, 1, 1));
+    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(3, 1, 1));
+    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(4, 1, 1));
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 4);
+}
+
+TEST(TileTest, TileRemovingAnimals) {
     Tile tile = Tile(1, 1);
     EXPECT_EQ(tile.getAnimalsOnTile()->size(), 0);
     std::shared_ptr<Animal> firstAnimal = std::make_shared<HerbivoreAnimal>(1, 1, 1);
     tile.addAnimalOnTile(firstAnimal);
-    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 1);
-    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(1, 1, 1));
-    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(1, 1, 1));
-    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(1, 1, 1));
-    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 4);
+    tile.addAnimalOnTile(std::make_shared<HerbivoreAnimal>(2, 1, 1));
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 2);
     tile.removeAnimalOnTile(firstAnimal);
-    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 3);
+    EXPECT_EQ(tile.getAnimalsOnTile()->size(), 1);
+    EXPECT_EQ(tile.getAnimalsOnTile()->at(0)->getId(), 2);
 }
 
+TEST(TileTest, TileAddingResources) {
+    Tile tile = Tile(1, 1);
+
+    EXPECT_EQ(tile.getResourcesOnTile()->size(), 0);
+
+    std::shared_ptr<Resource> firstResource = std::make_shared<Resource>(GRASS, 0, 0); // Assuming Resource takes an amount parameter
+    tile.addResourceOnTile(firstResource);
+    EXPECT_EQ(tile.getResourcesOnTile()->size(), 1);
+
+    tile.addResourceOnTile(std::make_shared<Resource>(GRASS, 0, 0));
+    tile.addResourceOnTile(std::make_shared<Resource>(GRASS, 0, 0));
+    tile.addResourceOnTile(std::make_shared<Resource>(GRASS, 0, 0));
+
+    EXPECT_EQ(tile.getResourcesOnTile()->size(), 1);
+
+    std::shared_ptr<Resource> differentResource = std::make_shared<Resource>(MEAT, 10, 10);
+    tile.addResourceOnTile(differentResource);
+    EXPECT_EQ(tile.getResourcesOnTile()->size(), 2);
+
+    auto resources = tile.getResourcesOnTile();
+    EXPECT_NE(std::find(resources->begin(), resources->end(), firstResource), resources->end());
+}
